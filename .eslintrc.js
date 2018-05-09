be821fc700e917e4de94f5fef4a1f443195795e4
venkatshenll <venkatesh.shenll@gmail.com>
2018-05-09 17:29:10 +0530

    eslint config update

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,6 @@ module.exports = {
     'es6': true
   },
   'extends': 'eslint:recommended',
-  'parserOptions': {
-    'sourceType': 'module'
-  },
   'plugins': ['node'],
   'rules': {
     'indent': [2, 4],
